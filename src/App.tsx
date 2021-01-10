5db105b0a2841aa7a7f5297e3ecf584728f5c031
Brent Marquez <brentonmarquez@gmail.com>
2021-01-10 12:42:30 -0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "materialize-css/dist/css/materialize.min.css";
+import "./styles/base.scss";
+
+const App: React.FC = () => {
+	return (
+		<div className="app-container">
+			<p>My App</p>
+		</div>
+	);
+};
+
+export default App;
